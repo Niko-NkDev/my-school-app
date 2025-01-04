dcfeca89ddb8b68078fc146b904d2f50fd98c1d2
Nicolas Cortes <128397932+Niko-NkDev@users.noreply.github.com>
2025-01-04 17:35:48 -0500

    Update App.js
    Importacion del footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import Menu from './components/shareds/Menu'; 
-import Footer from './components/shareds/Footer';
+
 function App() {
   return (
     <div className="App">
