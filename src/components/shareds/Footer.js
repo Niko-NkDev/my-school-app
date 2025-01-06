f4e29fd5de72166ea41b843566457eca56a142d0
Niko-NkDev <jncg20@gmail.com>
2025-01-06 18:08:33 -0500

    se termina menu, se pone un background img,

--- src/components/shareds/Footer.js
+++ src/components/shareds/Footer.js
@@ -1,47 +1,69 @@
 import React from "react";
 
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer style={styles.footer}>
       <div style={styles.iconsContainer}>
-        <a href="https://www.facebook.com/Andresvelasquezgarcia" target="_blank" rel="noopener noreferrer" style={styles.icon}>
-          <FaFacebook size={30} color= "black" />
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={styles.icon}
+        >
+          <FaFacebook size={30} color="black" />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" style={styles.icon}>
-          <FaTwitter size={30} color= "black" />
+        <a
+          href="https://twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={styles.icon}
+        >
+          <FaTwitter size={30} color="black" />
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" style={styles.icon}>
-          <FaInstagram size={30} color= "black" />
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={styles.icon}
+        >
+          <FaInstagram size={30} color="black" />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" style={styles.icon}>
-          <FaLinkedin size={30} color= "black" />
+        <a
+          href="https://linkedin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={styles.icon}
+        >
+          <FaLinkedin size={30} color="black" />
         </a>
       </div>
-      <p style={styles.text}>© 2025 My School App. Todos los derechos reservados.</p>
+      <p style={styles.text}>
+        © 2025 My School App. Todos los derechos reservados.
+      </p>
     </footer>
   );
 };
 
 const styles = {
   footer: {
-    backgroundColor: '#282c34',
-    color: '#fff',
-    textAlign: 'center',
-    padding: '20px 0',
+    backgroundColor: "#fff",
+    color: "#fff",
+    textAlign: "center",
+    padding: "20px 0",
   },
   iconsContainer: {
-    marginBottom: '10px',
+    marginBottom: "10px",
   },
   icon: {
-    margin: '0 10px',
-    color: '#fff',
-    textDecoration: 'none',
+    margin: "0 10px",
+    color: "#fff",
+    textDecoration: "none",
   },
   text: {
-    fontSize: '14px',
-    color: "black", 
+    fontSize: "14px",
+    color: "black",
   },
 };
 
--- src/components/shareds/Menu.js
+++ src/components/shareds/Menu.js
@@ -1,37 +1,65 @@
-import React from 'react';
-import logoMenu from '../../images/escudo-back-transparent.png';
-import { Link } from 'react-router-dom';
-import '../../styles/Menu.css';
+import React, { useState } from "react";
+import { FaGithub } from "react-icons/fa";
+import logoMenu from "../../images/escudo-back-transparent.png";
+import { Link } from "react-router-dom";
+import "../../styles/Menu.css";
 
 function Menu() {
+  // Estado para controlar el despliegue del menú hamburguesa
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Función para alternar el menú hamburguesa
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className='menu-container'>
-      <div className='logo-menu'>
-        <Link to='/'>
-          {' '}
-          <img className='logo-my-school' src={ logoMenu } alt='' />
+    <div className="menu-container">
+      <div className="logo-menu">
+        <Link to="/">
+          {" "}
+          <img className="logo-my-school" src={logoMenu} alt="" />
         </Link>
       </div>
 
-      {/* Botones del menú de navegación */}
+    
+    
+   
 
-      <div className='botones-menu'>
+      <div className="botones-menu">
+        <a href="https://github.com/Niko-NkDev"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+      <FaGithub size={30} color="black" />
+      </a>
         <nav>
-          <ul>
+          <ul className="menu">
             <li>
-              <Link to='/'>Inicio</Link>
+              <Link to="/">Inicio</Link>
             </li>
             <li>
-              <Link to='login'>Login</Link>
+              <Link to="nosotros">Nosotros</Link>
             </li>
+
             <li>
-              <Link to='cursos'>Cursos</Link>
-            </li>
-            <li>
-              <Link to='nosotros'>Nosotros</Link>
-            </li>
-            <li>
-              <Link to='contacto'>Contacto</Link>
+              <button className="hamburger-button" onClick={toggleMenu}>
+                Más Opciones
+              </button>
+
+              {isMenuOpen && (
+                <ul className="submenu">
+                  <li>
+                    <Link to="login">Login</Link>
+                  </li>
+                  <li>
+                    <Link to="cursos">Cursos</Link>
+                  </li>
+                  <li>
+                    <Link to="contacto">Contacto</Link>
+                  </li>
+                </ul>
+              )}
             </li>
           </ul>
         </nav>
